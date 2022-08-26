58d0a6cd371ecea63d4bfd2280fa3f61e21c1c7f
davidterpay <david.terpay@gmail.com>
2022-08-26 15:25:27 -0500

    Adding keys that will be used to access the keeper stoer

--- x/csr/types/keys.go
+++ x/csr/types/keys.go
@@ -1,5 +1,11 @@
 package types
 
+import (
+	"strconv"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
 const (
 	// ModuleName defines the module name
 	ModuleName = "csr"
@@ -9,18 +15,29 @@ const (
 
 	// RouterKey is the message route for slashing
 	RouterKey = ModuleName
-
-    // QuerierRoute defines the module's query routing key
-    QuerierRoute = ModuleName
-
-	// MemStoreKey defines the in-memory store key
-	MemStoreKey = "mem_csr"
-
-    
 )
 
+const (
+	// csr pool address -> csr struct
+	prefixCSR = iota + 1
+	// smartContract -> csr pool address
+	prefixSmartContract
+	// deployer address -> csr pool addresses
+	prefixDeployer
+	// smart contract address + NFT id -> last withdraw period
+	prefixLastPeriodNFT
+)
 
+// KVStore key prefixes
+var (
+	KeyPrefixCSR           = []byte{prefixCSR}
+	KeyPrefixSmartContract = []byte{prefixSmartContract}
+	KeyPrefixDeployer      = []byte{prefixDeployer}
+	KeyPrefixLastPeriodNFT = []byte{prefixLastPeriodNFT}
+)
 
-func KeyPrefix(p string) []byte {
-    return []byte(p)
+// Multi-tier index to find the latest withdrawal period for an NFT
+func GetKeyPrefixLastPeriodNFT(contractAddress common.Address, id uint64) []byte {
+	contractPrefix := append(KeyPrefixLastPeriodNFT, contractAddress.Bytes()...)
+	return append(contractPrefix, []byte(strconv.FormatUint(id, 10))...)
 }
