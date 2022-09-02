928a02e70a76a70798a921f0d98d81af81e13b6d
davidterpay <david.terpay@gmail.com>
2022-09-02 17:13:01 -0500

    removing module addr

--- contracts/csr.go
+++ contracts/csr.go
@@ -4,23 +4,18 @@ import (
 	_ "embed" // embed compiled smart contract
 	"encoding/json"
 
-	"github.com/ethereum/go-ethereum/common"
-
-	"github.com/Canto-Network/Canto/v2/x/csr/types"
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
 )
 
 var (
 	//go:embed compiled_contracts/Turnstile.json
-	TurnstileJSON            []byte
-	TurnstileContract        evmtypes.CompiledContract
-	TurnstileContractAddress common.Address
+	TurnstileJSON     []byte
+	TurnstileContract evmtypes.CompiledContract
 
 	// TODO: Need to add the CSR NFT code here once written
 )
 
 func init() {
-	TurnstileContractAddress = types.ModuleAddress
 
 	err := json.Unmarshal(TurnstileJSON, &TurnstileContract)
 	if err != nil {
