85fab7ae824e7bf9b0c816fc44172e5e8622536e
Nikhil <nikhil@plex.engineer>
2022-09-03 16:19:47 -0400

    add CSRNFT contract + tests

--- x/csr/types/errors.go
+++ x/csr/types/errors.go
@@ -15,10 +15,4 @@ var (
 	ErrPrevRegisteredSmartContract = sdkerrors.Register(ModuleName, 1006, "csr::CSR")
 
 	ErrInvalidParams = sdkerrors.Register(ModuleName, 1003, "csr::Params")
-
-	// keeper errors
-	ErrAddressDerivation   = sdkerrors.Register(ModuleName, 1004, "csr::Keeper")
-	ErrContractDeployments = sdkerrors.Register(ModuleName, 1008, "csr::Keeper")
-	ErrAddressDerivation   = sdkerrors.Register(ModuleName, 1005, "csr::Keeper")
-	ErrContractDeployments = sdkerrors.Register(ModuleName, 1007, "csr::Keeper")
 )
