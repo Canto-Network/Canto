8bef029411770f64b08af86a84571d03ff4b18cf
Jacob Gadikian <jacobgadikian@gmail.com>
2023-02-06 22:32:10 +0700

    protobufs

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-
 set -eo pipefail
-
 
 protoc_gen_gocosmos() {
   if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
@@ -10,46 +8,49 @@ protoc_gen_gocosmos() {
     return 1
   fi
 
-  go install  github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
-
+  go get github.com/regen-network/cosmos-proto/protoc-gen-gocosmos 2>/dev/null
 }
 
 protoc_gen_doc() {
-  go install github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc 2>/dev/null
+  go get -u github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc 2>/dev/null
 }
-
-echo "1"
 
 protoc_gen_gocosmos
 protoc_gen_doc
 
-echo "2"
+# need to install statik on the docker image
+go install github.com/rakyll/statik
 
-proto_dirs=$(find ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
-# TODO: migrate to `buf build`
+# create temporary folder to store intermediate results from `buf build` + `buf generate`
+mkdir -p ./tmp-swagger-gen
+
+# build .proto files and generate code for the proto/ directory
+buf build proto
+buf generate proto --template buf.gen.proto.yaml
+
+# create additional swagger files on an individual basis  w/ `buf build` and `buf generate` (needed for `swagger-combine`)
+proto_dirs=$(find ./proto ./third_party/proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  buf alpha protoc \
-  -I "proto" \
-  -I "third_party/proto" \
-  --gocosmos_out=plugins=interfacetype+grpc,\
-Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types:. \
-  --grpc-gateway_out=logtostderr=true:. \
-  $(find "${dir}" -maxdepth 1 -name '*.proto')
 
+  # generate swagger files (filter query files)
+  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
+  if [[ ! -z "$query_file" ]]; then
+    buf build --path "$query_file"
+    buf generate --path "$query_file" --template buf.gen.swagger.yaml
+  fi
 done
 
-echo "3"
-
-# command to generate docs using protoc-gen-doc
-# TODO: migrate to `buf build`
-# buf alpha protoc \
-# -I "proto" \
-# -I "third_party/proto" \
-# --doc_out=./docs/protocol \
-# --doc_opt=./docs/protodoc-markdown.tmpl,proto-docs.md \
-# $(find "$(pwd)/proto" -maxdepth 5 -name '*.proto')
-
-
-# move proto files to the right places
-cp -r github.com/Canto-Network/Canto/v1/x/* x/
+# move resulting files to the right places
+cp -r github.com/evmos/evmos/v*/x/* x/
 rm -rf github.com
+
+# combine swagger files
+# uses nodejs package `swagger-combine`.
+# all the individual swagger files need to be configured in `config.json` for merging
+swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
+
+# clean swagger files
+rm -rf ./tmp-swagger-gen
+
+# generate binary for static server (use -f flag to replace current binary)
+statik -f -src=./client/docs/swagger-ui -dest=./client/docs
