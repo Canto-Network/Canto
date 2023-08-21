ba1c02ff6de03c4476b16b26a22106a4effd7d57
dongsam <dongsamb@gmail.com>
2023-08-21 18:02:22 +0900

    feat: update antehandler to cover nested authz msg cases

--- app/ante/param_change_ante_test.go
+++ app/ante/param_change_ante_test.go
@@ -1,18 +1,22 @@
 package ante_test
 
 import (
+	"fmt"
 	"strconv"
 
-	"github.com/Canto-Network/Canto/v7/app/ante"
-	"github.com/Canto-Network/Canto/v7/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/cosmos/cosmos-sdk/x/params/types/proposal"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+
+	"github.com/Canto-Network/Canto/v7/app/ante"
+	authzante "github.com/Canto-Network/Canto/v7/app/ante/cosmos"
+	"github.com/Canto-Network/Canto/v7/types"
 )
 
-// TODO: Advanced test cases (e.g. nested param change proposals)
-// Authz and multi msg cases
+// single param change msg cases
 func (suite *AnteTestSuite) TestParamChangeProposal() {
 	suite.SetupTest(false)
 	params := suite.app.SlashingKeeper.GetParams(suite.ctx)
@@ -114,8 +118,8 @@ func (suite *AnteTestSuite) TestParamChangeProposal() {
 		{
 			"Changing BondDenom is not allowed",
 			func() *proposal.ParameterChangeProposal {
-				unbondingTime := proposal.NewParamChange("staking", "BondDenom", "adoge")
-				return proposal.NewParameterChangeProposal("tc11", "tc11", []proposal.ParamChange{unbondingTime})
+				bondDenomChange := proposal.NewParamChange("staking", "BondDenom", "adoge")
+				return proposal.NewParameterChangeProposal("tc11", "tc11", []proposal.ParamChange{bondDenomChange})
 			},
 			types.ErrChangingBondDenomForbidden,
 		},
@@ -131,6 +135,173 @@ func (suite *AnteTestSuite) TestParamChangeProposal() {
 				suite.addr,
 			)
 			tx, err := createTx(suite.priv, []sdk.Msg{msg}...)
+			suite.Require().NoError(err)
+			_, err = anteHandler(suite.ctx, tx, false)
+			if tc.expectedError != nil {
+				suite.ErrorContains(err, tc.expectedError.Error())
+			} else {
+				suite.Require().NoError(err)
+			}
+		})
+	}
+}
+
+// authz, nested, multi msg cases
+func (suite *AnteTestSuite) TestAuthzCases() {
+	suite.SetupTest(false)
+
+	bondDenomChange := proposal.NewParamChange("staking", "BondDenom", "adoge")
+	coins := sdk.NewCoins(sdk.NewCoin(suite.app.StakingKeeper.BondDenom(suite.ctx), sdk.NewInt(10000)))
+	normMsg := &banktypes.MsgSend{
+		FromAddress: suite.addr.String(),
+		ToAddress:   suite.addr.String(),
+		Amount:      coins,
+	}
+	limitedMsg, err := govtypes.NewMsgSubmitProposal(
+		proposal.NewParameterChangeProposal("tc11", "tc11", []proposal.ParamChange{bondDenomChange}),
+		coins,
+		suite.addr,
+	)
+	suite.NoError(err)
+
+	wrapAuthzMsg := func(msg sdk.Msg) *authz.MsgExec {
+		v := authz.NewMsgExec(suite.addr, []sdk.Msg{msg})
+		return &v
+	}
+	authzMsg := authz.NewMsgExec(suite.addr, []sdk.Msg{limitedMsg})
+	authzMultiMsg := authz.NewMsgExec(suite.addr, []sdk.Msg{normMsg, limitedMsg})
+
+	tests := []struct {
+		desc          string
+		msgs          []sdk.Msg
+		expectedError error
+	}{
+		{
+			"normal msg",
+			[]sdk.Msg{normMsg},
+			nil,
+		},
+		{
+			"limited msg",
+			[]sdk.Msg{limitedMsg},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"multi msg-1",
+			[]sdk.Msg{normMsg, limitedMsg},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"multi msg-2",
+			[]sdk.Msg{limitedMsg, normMsg},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"authz msg-1",
+			[]sdk.Msg{&authzMsg},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"authz msg-2",
+			[]sdk.Msg{normMsg, &authzMsg},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"authz msg-3",
+			[]sdk.Msg{&authzMsg, normMsg},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"authz msg-4",
+			[]sdk.Msg{&authzMultiMsg},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"authz msg-5",
+			[]sdk.Msg{normMsg, &authzMultiMsg},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"nested authz msg-1",
+			[]sdk.Msg{
+				wrapAuthzMsg(
+					wrapAuthzMsg(
+						&authzMsg),
+				),
+			},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"nested authz msg-2",
+			[]sdk.Msg{
+				wrapAuthzMsg(
+					wrapAuthzMsg(
+						wrapAuthzMsg(
+							wrapAuthzMsg(
+								&authzMsg),
+						),
+					),
+				),
+			},
+			types.ErrChangingBondDenomForbidden,
+		},
+		{
+			"nested authz msg-3",
+			[]sdk.Msg{
+				wrapAuthzMsg(
+					wrapAuthzMsg(
+						wrapAuthzMsg(
+							wrapAuthzMsg(
+								normMsg),
+						),
+					),
+				),
+			},
+			nil,
+		},
+		{
+			"nested authz msg-3",
+			[]sdk.Msg{
+				wrapAuthzMsg(
+					wrapAuthzMsg(
+						wrapAuthzMsg(
+							wrapAuthzMsg(
+								wrapAuthzMsg(
+									wrapAuthzMsg(
+										&authzMsg),
+								),
+							),
+						),
+					),
+				),
+			},
+			fmt.Errorf("found more nested msgs than permited. Limit is : %d", authzante.MaxNestedMsgs),
+		},
+		{
+			"nested authz msg-4",
+			[]sdk.Msg{
+				wrapAuthzMsg(
+					wrapAuthzMsg(
+						wrapAuthzMsg(
+							wrapAuthzMsg(
+								wrapAuthzMsg(
+									wrapAuthzMsg(
+										normMsg),
+								),
+							),
+						),
+					),
+				),
+			},
+			fmt.Errorf("found more nested msgs than permited. Limit is : %d", authzante.MaxNestedMsgs),
+		},
+	}
+
+	spcld := ante.NewParamChangeLimitDecorator(&suite.app.SlashingKeeper, suite.app.AppCodec())
+	anteHandler := sdk.ChainAnteDecorators(spcld)
+	for _, tc := range tests {
+		suite.Run(tc.desc, func() {
+			tx, err := createTx(suite.priv, tc.msgs...)
 			suite.Require().NoError(err)
 			_, err = anteHandler(suite.ctx, tx, false)
 			if tc.expectedError != nil {
