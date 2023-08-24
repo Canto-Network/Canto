3b237a154b404fdf5aa264fad2d81db8e2a72c61
Byungchul Park <125338011+poorphd@users.noreply.github.com>
2023-08-25 04:49:23 +0900

    fix: genesis import/export for govshuttle module (#50)
    * fix: genesis import/export for govshuttle module
    
    * fix: empty port address bug
    
    * test: add assertion for empty port when govshuttle genesis import, export
    
    ---------
    
    Co-authored-by: dongsam <dongsamb@gmail.com>

--- x/govshuttle/keeper/keeper.go
+++ x/govshuttle/keeper/keeper.go
@@ -7,10 +7,11 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/tendermint/tendermint/libs/log"
 
-	"github.com/Canto-Network/Canto/v7/x/govshuttle/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+
+	"github.com/Canto-Network/Canto/v7/x/govshuttle/types"
 )
 
 type (
--- x/govshuttle/keeper/keeper_test.go
+++ x/govshuttle/keeper/keeper_test.go
@@ -4,8 +4,14 @@ import (
 	"encoding/json"
 	"math/big"
 	"testing"
+	"time"
 
+	"github.com/evmos/ethermint/crypto/ethsecp256k1"
 	"github.com/stretchr/testify/suite"
+	"github.com/tendermint/tendermint/crypto/tmhash"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmversion "github.com/tendermint/tendermint/proto/tendermint/version"
+	"github.com/tendermint/tendermint/version"
 
 	"github.com/Canto-Network/Canto/v7/app"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
@@ -53,6 +59,42 @@ func (suite *KeeperTestSuite) DoSetupTest(t require.TestingT) {
 
 	//init app
 	suite.app = app.Setup(checkTx, feemarketGenesis)
+
+	priv, err := ethsecp256k1.GenerateKey()
+	require.NoError(t, err)
+
+	suite.address = common.BytesToAddress(priv.PubKey().Address().Bytes())
+
+	// consensus key
+	privCons, err := ethsecp256k1.GenerateKey()
+	require.NoError(t, err)
+	consAddress := sdk.ConsAddress(privCons.PubKey().Address())
+
+	// setup context
+	suite.ctx = suite.app.BaseApp.NewContext(checkTx, tmproto.Header{
+		Height:          1,
+		ChainID:         "canto_9001-1",
+		Time:            time.Now().UTC(),
+		ProposerAddress: consAddress.Bytes(),
+
+		Version: tmversion.Consensus{
+			Block: version.BlockProtocol,
+		},
+		LastBlockId: tmproto.BlockID{
+			Hash: tmhash.Sum([]byte("block_id")),
+			PartSetHeader: tmproto.PartSetHeader{
+				Total: 11,
+				Hash:  tmhash.Sum([]byte("partset_header")),
+			},
+		},
+		AppHash:            tmhash.Sum([]byte("app")),
+		DataHash:           tmhash.Sum([]byte("data")),
+		EvidenceHash:       tmhash.Sum([]byte("evidence")),
+		ValidatorsHash:     tmhash.Sum([]byte("validators")),
+		NextValidatorsHash: tmhash.Sum([]byte("next_validators")),
+		ConsensusHash:      tmhash.Sum([]byte("consensus")),
+		LastResultsHash:    tmhash.Sum([]byte("last_result")),
+	})
 }
 
 func (suite *KeeperTestSuite) SetupTest() {
