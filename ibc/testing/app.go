f2ad9c58b96cea17a005a0402db2c7ee1cf4b2f8
poorphd <bc.park@bharvest.io>
2023-12-20 15:07:18 +0900

    wip: error fix in Canto modules

--- ibc/testing/app.go
+++ ibc/testing/app.go
@@ -186,7 +186,7 @@ func SetupWithGenesisValSetCanto(tb testing.TB, valSet *tmtypes.ValidatorSet, ge
 			MinSelfDelegation: sdkmath.ZeroInt(),
 		}
 		validators = append(validators, validator)
-		delegations = append(delegations, stakingtypes.NewDelegation(genAccs[0].GetAddress().String(), val.Address.String(), sdkmath.LegacyOneDec()))
+		delegations = append(delegations, stakingtypes.NewDelegation(genAccs[0].GetAddress().String(), sdk.ValAddress(val.Address).String(), sdkmath.LegacyOneDec()))
 	}
 
 	// set validators and delegations
