c68de59c2d721c34a1b0539ea9a17d9e770706cd
poorphd <bc.park@bharvest.io>
2024-07-10 23:31:01 +0900

    fix: test function modified

--- app/signer_test.go
+++ app/signer_test.go
@@ -5,13 +5,9 @@ import (
 
 	"cosmossdk.io/x/tx/signing"
 	coinswapapi "github.com/Canto-Network/Canto/v7/api/canto/coinswap/v1"
-	erc20v1 "github.com/Canto-Network/Canto/v7/api/canto/erc20/v1"
 	coinswaptypes "github.com/Canto-Network/Canto/v7/x/coinswap/types"
-	erc20types "github.com/Canto-Network/Canto/v7/x/erc20/types"
 	"github.com/cosmos/cosmos-sdk/codec/address"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	evmv1 "github.com/evmos/ethermint/api/ethermint/evm/v1"
-	evmtypes "github.com/evmos/ethermint/x/evm/types"
 	"github.com/stretchr/testify/require"
 	protov2 "google.golang.org/protobuf/proto"
 )
@@ -19,6 +15,8 @@ import (
 func TestMsgSwapOrderSigners(t *testing.T) {
 
 	addr := "canto13e9t6s6ra8caz5zzmy5w9v23dm2dr5nrr9sz03"
+	accAddr, err := sdk.AccAddressFromBech32(addr)
+	require.NoError(t, err)
 
 	signingOptions := signing.Options{
 		AddressCodec: address.Bech32Codec{
@@ -29,8 +27,6 @@ func TestMsgSwapOrderSigners(t *testing.T) {
 		},
 	}
 	signingOptions.DefineCustomGetSigners(protov2.MessageName(&coinswapapi.MsgSwapOrder{}), coinswaptypes.CreateGetSignersFromMsgSwapOrderV2(&signingOptions))
-	signingOptions.DefineCustomGetSigners(protov2.MessageName(&evmv1.MsgEthereumTx{}), evmtypes.GetSignersFromMsgEthereumTxV2)
-	signingOptions.DefineCustomGetSigners(protov2.MessageName(&erc20v1.MsgConvertERC20{}), erc20types.GetSignersFromMsgConvertERC20V2)
 
 	ctx, err := signing.NewContext(signingOptions)
 	require.NoError(t, err)
@@ -46,7 +42,7 @@ func TestMsgSwapOrderSigners(t *testing.T) {
 			msg: &coinswapapi.MsgSwapOrder{
 				Input: &coinswapapi.Input{Address: addr},
 			},
-			want: [][]byte{[]byte(sdk.AccAddress(addr))},
+			want: [][]byte{accAddr},
 		},
 	}
 	for _, test := range tests {
